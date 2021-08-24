351fe84362d5546d62524bfcf5a57b96adbbee63
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2021-08-24 10:55:29 -0300

    Finalizado e testado o desafio - passou em todos os testes do jest

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,24 +5,48 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface TransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  private balance: Balance = {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  };
 
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(transaction: TransactionDTO): Transaction {
+    if (transaction.type === 'outcome') {
+      this.balance.outcome += transaction.value;
+      this.balance.total -= transaction.value;
+    } else {
+      this.balance.income += transaction.value;
+      this.balance.total += transaction.value;
+    }
+
+    const newTransaction = new Transaction(transaction);
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,13 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +15,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const transaction = {
+      title,
+      value,
+      type,
+    };
+
+    if (transaction.type === 'outcome') {
+      const totalBalance = this.transactionsRepository.getBalance().total;
+      if (totalBalance < transaction.value) {
+        throw Error('The value exceeds account value');
+      }
+    }
+    const newTransaction = this.transactionsRepository.create(transaction);
+
+    return newTransaction;
   }
 }
 
